90cb279cf83a0ad891b98f90d8cf730f86cfa803
lebrunregis <lebrun.regis.formation@gmail.com>
2026-05-18 16:56:58 +0200

    Extracted ingredient table

--- src/alchemy_db/__init__.py
+++ src/alchemy_db/__init__.py
@@ -17,7 +17,22 @@ if(os.getenv('FIRST_TIME_INITIALIZE')):
                 session.commit()
 
         # Load Excel file
-        df = pd.read_csv('recipes_w_search_terms.csv')
+        recipes = pd.read_csv('recipes_w_search_terms.csv')
 
-        # Import DataFrame into PostgreSQL
-        df.to_sql('recipes', engine, if_exists='replace', index=False)
+        # If ingredients are stored as strings like "['salt', 'pepper']"
+        recipes['ingredients'] = recipes['ingredients'].apply(ast.literal_eval)
+
+        # Flatten all ingredient lists into one list
+        all_ingredients = [
+        ingredient
+        for sublist in recipes['ingredients']
+                for ingredient in sublist
+        ]
+        # Remove duplicates
+        unique_ingredients = pd.DataFrame({
+                'ingredient': pd.unique(all_ingredients)
+        })
+        recipe_ingredients = 
+        # Import DataFrames into PostgreSQL
+        unique_ingredients.to_sql('ingredients', engine, if_exists='replace', index=False)  
+        recipes.to_sql('recipes', engine, if_exists='replace', index=False)
