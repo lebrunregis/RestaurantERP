75926f68d49ed1da8312f2c1c0e57dd841401f3a
lebrunregis <lebrun.regis.formation@gmail.com>
2026-05-19 10:41:29 +0200

    Added basic import of ingredient table and created join table

--- src/alchemy_db/__init__.py
+++ src/alchemy_db/__init__.py
@@ -1,5 +1,5 @@
 import os
-
+from urllib.parse import unquote
 import pandas as pd
 import ast
 from .models.base_model import Base
@@ -17,9 +17,8 @@ if(os.getenv('FIRST_TIME_INITIALIZE')):
                 session.commit()
 
         # Load Excel file
-        recipes = pd.read_csv('recipes_w_search_terms.csv')
-
-        # If ingredients are stored as strings like "['salt', 'pepper']"
+        recipes :pd.DataFrame = pd.read_csv('recipes_w_search_terms.csv')
+        # transform the string into an array of strings safely without using eval
         recipes['ingredients'] = recipes['ingredients'].apply(ast.literal_eval)
 
         # Flatten all ingredient lists into one list
@@ -30,9 +29,31 @@ if(os.getenv('FIRST_TIME_INITIALIZE')):
         ]
         # Remove duplicates
         unique_ingredients = pd.DataFrame({
-                'ingredient': pd.unique(all_ingredients)
+                'name': pd.unique(all_ingredients)
         })
-        recipe_ingredients = 
+        unique_ingredients['ingredient_id'] = range(1, len(unique_ingredients) + 1)
+        # Fix text formatting errors
+        unique_ingredients['name'] = unique_ingredients['name'].apply(unquote)
+        # Create lookup dictionary
+        ingredient_lookup = dict(zip(unique_ingredients['name'], unique_ingredients['ingredient_id']))
+
+        # Build join table
+        recipe_ingredient_list = []
+
+        for _, row in recipes.iterrows():
+                recipe_id = row['id']
+                for ingredient in row['ingredients']:
+                        recipe_ingredient_list.append({
+                        'recipe_id': recipe_id,
+                        'ingredient_id': ingredient_lookup[unquote(ingredient)]
+                        })
+        # Dataframe conversion
+        recipe_ingredients = pd.DataFrame(recipe_ingredient_list)
+        # Remove unnecessary data
+        recipes.drop(columns=['ingredients'], inplace=True)
+        # Rename columns for clarity
+        recipes.rename(columns={'id':"recipe_id"})
         # Import DataFrames into PostgreSQL
-        unique_ingredients.to_sql('ingredients', engine, if_exists='replace', index=False)  
-        recipes.to_sql('recipes', engine, if_exists='replace', index=False)
+        unique_ingredients.to_sql('ingredients', engine, if_exists='append', index=False)  
+        recipes.to_sql('recipes', engine, if_exists='append', index=False)
+        recipe_ingredients.to_sql('recipe_ingredients', engine, if_exists='append', index=False)
