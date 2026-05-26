202c3b2d3911df230b919f4c9a4478cf74036b9c
lebrunregis <lebrun.regis.formation@gmail.com>
2026-05-26 16:58:15 +0200

    Replaced init by @dataclass annotation

--- src/adapters_logic/dtos/recipe_dto.py
+++ src/adapters_logic/dtos/recipe_dto.py
@@ -1,21 +1,15 @@
 # adapters_logic/dtos/recipe_dto.py
-from typing import List
+from dataclasses import dataclass
+from typing import  Optional
 
+@dataclass
 class RecipeDTO:
-    def __init__(
-        self,
-        recipe_id: int,
-        name: str,
-        description: str,
-        ingredients: List[str],
-        serving_size_grams: int,
-        servings: int,
-        steps: List[str]
-    ):
-        self.recipe_id = recipe_id
-        self.name = name
-        self.description = description
-        self.ingredients = ingredients
-        self.serving_size_grams = serving_size_grams
-        self.servings = servings
-        self.steps = steps
+        recipe_id:Optional[int]
+        name:str
+        description:str
+        ingredients :list[str]
+        serving_size_grams:int
+        servings :int
+        steps:str
+
+
